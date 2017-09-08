5e169893b4827f50961aee744d0933257aae1e4d
Nick Johnson <nickjohnson.dev@gmail.com>
2017-09-08 17:17:56 -0500

    feat: Add sample song. Add lodash+tone deps.

---
+++ examples/sample-song.js
@@ -0,0 +1,80 @@
+export default {
+  bpm: 120,
+  id: '0',
+  measureCount: 4,
+  notes: {
+    '0': {
+      id: '0',
+      points: [{ x: 0, y: 47 }, { x: 1, y: 47 }],
+      sequenceId: '0'
+    },
+    '1': {
+      id: '1',
+      points: [{ x: 0, y: 35 }, { x: 1, y: 35 }],
+      sequenceId: '1'
+    },
+    '2': {
+      id: '2',
+      points: [{ x: 0, y: 23 }, { x: 1, y: 23 }],
+      sequenceId: '2'
+    },
+    '3': {
+      id: '3',
+      points: [{ x: 0, y: 11 }, { x: 1, y: 11 }],
+      sequenceId: '3'
+    },
+  },
+  sequences: {
+    '0': {
+      id: '0',
+      measureCount: 1,
+      position: 0,
+      trackId: '0',
+    },
+    '1': {
+      id: '1',
+      measureCount: 1,
+      position: 1,
+      trackId: '1',
+    },
+    '2': {
+      id: '2',
+      measureCount: 1,
+      position: 2,
+      trackId: '2',
+    },
+    '3': {
+      id: '3',
+      measureCount: 1,
+      position: 3,
+      trackId: '3',
+    },
+  },
+  title: 'Sample Song',
+  tracks: {
+    '0': {
+      id: '0',
+      isMuted: false,
+      isSoloing: false,
+      voice: 'sine',
+    },
+    '1': {
+      id: '1',
+      isMuted: false,
+      isSoloing: false,
+      voice: 'sine',
+    },
+    '2': {
+      id: '2',
+      isMuted: false,
+      isSoloing: false,
+      voice: 'sine',
+    },
+    '3': {
+      id: '3',
+      isMuted: false,
+      isSoloing: false,
+      voice: 'sine',
+    },
+  },,
+};
