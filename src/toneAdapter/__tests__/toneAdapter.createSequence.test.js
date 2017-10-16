b587bde3f5842df243509e955ed537cadb07b332
Nick Johnson <nickjohnson.dev@gmail.com>
2017-10-16 15:51:27 -0500

    test: Add missing change to toneAdapter.createSequence test

--- src/toneAdapter/__tests__/toneAdapter.createSequence.test.js
+++ src/toneAdapter/__tests__/toneAdapter.createSequence.test.js
@@ -1,5 +1,4 @@
 import test from 'ava';
-import noop from 'lodash/fp/noop';
 import sinon from 'sinon';
 import { createToneAdapter } from '../index';
 
@@ -16,7 +15,7 @@ test('should return instance of Tone.Sequence ', (t) => {
 
 test('should invoke Tone.Sequence constructor with onStep, array of empty objects with length === length, "32n"', (t) => {
   const onStep = () => {};
-  const expected = [onStep, [{ fn: noop }, { fn: noop }, { fn: noop }], '32n'];
+  const expected = [onStep, [0, 1, 2], '32n'];
   const constructor = sinon.spy();
   class Sequence {
     constructor(...args) {
