86e2a931d07e27d18a3974a7a11182023ae13340
Nick Johnson <nickjohnson.dev@gmail.com>
2019-05-05 14:10:18 -0500

    build: Switch to yarn, add large sample song to test performance.

--- examples/index.js
+++ examples/index.js
@@ -1,85 +1,102 @@
 import { h } from 'picodom';
 import Dawww from '../src';
 import render from './render';
-import sampleSong from './sampleSong';
+import sampleSong from './loadTestingSong.json';
 import sampleSongAlt from './sampleSongAlt';
 
 const dawww = Dawww({
   song: sampleSong,
 });
 
-const view = props => h('div', {}, [
+const view = props =>
   h('div', {}, [
-    props.playbackState,
-  ]),
-  h('div', {
-    style: {
-      backgroundColor: '#ddd',
-      height: '20px',
-      position: 'relative',
-      width: `${sampleSong.measureCount * 64}px`,
-    },
-  }, [
-    h('div', {
-      style: {
-        left: `${2 * props.position}px`,
-        position: 'absolute',
-        top: 0,
-        width: '100px',
+    h('div', {}, [props.playbackState]),
+    h(
+      'div',
+      {
+        style: {
+          backgroundColor: '#ddd',
+          height: '20px',
+          position: 'relative',
+          width: `${sampleSong.measureCount * 64}px`,
+        },
       },
-    }, [
-      `| ${props.position}`,
+      [
+        h(
+          'div',
+          {
+            style: {
+              left: `${2 * props.position}px`,
+              position: 'absolute',
+              top: 0,
+              width: '100px',
+            },
+          },
+          [`| ${props.position}`],
+        ),
+      ],
+    ),
+    h('div', {}, [
+      h(
+        'button',
+        {
+          onmousedown: () => {
+            if (props.playbackState === 'STARTED') {
+              dawww.pause();
+            } else {
+              dawww.start();
+            }
+          },
+        },
+        [props.playbackState === 'STARTED' ? 'pause' : 'start'],
+      ),
+      h(
+        'button',
+        {
+          onmousedown: () => {
+            dawww.stop();
+          },
+        },
+        ['stop'],
+      ),
+      h(
+        'button',
+        {
+          onmousedown: () => {
+            dawww.updateSong(sampleSong);
+          },
+        },
+        ['load song'],
+      ),
+      h(
+        'button',
+        {
+          onmousedown: () => {
+            dawww.updateSong(sampleSongAlt);
+          },
+        },
+        ['update song'],
+      ),
+      h(
+        'button',
+        {
+          onmousedown: () => {
+            dawww.preview(0, 47);
+          },
+        },
+        ['play C3'],
+      ),
+      h(
+        'button',
+        {
+          onmousedown: () => {
+            dawww.setPosition(8);
+          },
+        },
+        ['set position 8'],
+      ),
     ]),
-  ]),
-  h('div', {}, [
-    h('button', {
-      onmousedown: () => {
-        if (props.playbackState === 'STARTED') {
-          dawww.pause();
-        } else {
-          dawww.start();
-        }
-      },
-    }, [
-      props.playbackState === 'STARTED' ? 'pause' : 'start',
-    ]),
-    h('button', {
-      onmousedown: () => {
-        dawww.stop();
-      },
-    }, [
-      'stop',
-    ]),
-    h('button', {
-      onmousedown: () => {
-        dawww.updateSong(sampleSong);
-      },
-    }, [
-      'load song',
-    ]),
-    h('button', {
-      onmousedown: () => {
-        dawww.updateSong(sampleSongAlt);
-      },
-    }, [
-      'update song',
-    ]),
-    h('button', {
-      onmousedown: () => {
-        dawww.preview(0, 47);
-      },
-    }, [
-      'play C3',
-    ]),
-    h('button', {
-      onmousedown: () => {
-        dawww.setPosition(8);
-      },
-    }, [
-      'set position 8',
-    ]),
-  ]),
-]);
+  ]);
 
 dawww.onPositionChange((position) => {
   render(view, { position });
